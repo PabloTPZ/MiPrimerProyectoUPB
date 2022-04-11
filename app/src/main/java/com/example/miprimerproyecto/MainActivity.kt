d092faf558908ab7d5544f5709ea6a0310668606
Pablo Perez <perez.zpt@gmail.com>
2022-04-11 09:07:28 -0400

    Update MainActivity.kt

--- app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
+++ app/src/main/java/com/example/miprimerproyecto/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("TAG", "onCreate:  aqui hay conflicto")
+        Log.d("TAG", "onCreate:  aqui un programa")
     }
 }
